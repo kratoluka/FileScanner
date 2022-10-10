8da464efbe8f9728a8a2427e4eb2625b82065947
kratoluka <kratoluka@gmail.com>
2022-10-10 19:08:13 +0200

    Add option to output detect to log

--- FileScanner.py
+++ FileScanner.py
@@ -24,9 +24,9 @@ def uncase(text):
     return ' '.join(text.split('_')).capitalize()
 
 
-def compare_files(verbose, file_name, file_old, file_new):
+def compare_files(verbose, file_name, file_old, file_new, log):
     if file_old != file_new:
-        print('Changes in file ' + file_name)
+        print('Changes in file: ' + file_name, file=log)
         if verbose > 0:
             for key in list(file_old.keys()):
                 if 'time' in key:
@@ -36,7 +36,7 @@ def compare_files(verbose, file_name, file_old, file_new):
                     old_value, new_value = file_old[key], file_new[key]
                 if old_value != file_new[key]:
                     print(f"{INDENT}Value {key} was changed" +
-                          (verbose > 1) * f" from {old_value} to {new_value}")
+                          (verbose > 1) * f" from {old_value} to {new_value}", file=log)
 
 
 def scan(directory, json_file):
@@ -49,7 +49,7 @@ def scan(directory, json_file):
         json.dump(metadata, f, indent=INDENT)
 
 
-def detect(arguments):
+def detect(arguments, log):
     with open(arguments.metadata, 'r', encoding='utf-8') as f:
         metadata = json.load(f)
         file_list = list(metadata.keys())
@@ -57,19 +57,24 @@ def detect(arguments):
             for file in files:
                 file_name = os.path.join(root, file)
                 if file_name not in file_list:
-                    print('New file: ' + file_name)
+                    print('New file: ' + file_name, file=log)
                 else:
                     file_list.remove(file_name)
-                    compare_files(arguments.verbose, file_name, metadata[file_name], file_info(file_name))
+                    compare_files(arguments.verbose, file_name, metadata[file_name], file_info(file_name), log)
         for file_name in file_list:
-            print('Removed file: ' + file_name)
+            print('Removed file: ' + file_name, file=log)
 
 
 def main(arguments):
     if arguments.run_option == 'scan':
         scan(arguments.directory, arguments.metadata)
     else:
-        detect(arguments)
+        if arguments.log:
+            with open(arguments.log, 'a') as log:
+                print('-'*20, file=log)
+                detect(arguments, log)
+        else:
+            detect(arguments, None)
 
 
 if __name__ == "__main__":
